f72ffdaa77b90b14894bf25a0e6904ca13f252f1
Mohamed Seedyk <mohamedseedk908@gmail.com>
2024-12-11 15:32:27 -0800

    first commit

---
+++ android/app/src/main/kotlin/com/ms/dalel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ms.dalel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Dalel());
+}
+
+class Dalel extends StatelessWidget {
+  const Dalel({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DalelApp',
+    );
+  }
+}
